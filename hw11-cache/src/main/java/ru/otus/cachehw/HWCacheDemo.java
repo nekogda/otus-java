2d3928350d2d2625f2ba00aaccfb7ba969100924
nekogda <nekogda@>
2020-09-06 18:49:53 +0300

    Fixing based on comments

--- hw11-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/HWCacheDemo.java
@@ -8,7 +8,6 @@ public class HWCacheDemo {
 
     public static void main(String[] args) {
         new HWCacheDemo().demo();
-        System.gc();
     }
 
     private void demo() {
@@ -18,21 +17,26 @@ public class HWCacheDemo {
         HwListener<Integer, Integer> listener = new HwListener<Integer, Integer>() {
             @Override
             public void notify(Integer key, Integer value, String action) {
+                throw new RuntimeException("AAA");
+            }
+        };
+
+        HwListener<Integer, Integer> listener2 = new HwListener<Integer, Integer>() {
+            @Override
+            public void notify(Integer key, Integer value, String action) {
                 logger.info("key: {}, value: {}, action: {}", key, value, action);
             }
         };
 
         cache.addListener(listener);
+        cache.addListener(listener2);
         cache.put(1, 1);
 
         logger.info("getValue: {}", cache.get(1));
 
         cache.remove(1);
-//        cache.removeListener(listener);
-        listener = null;
-        System.gc();
+        cache.removeListener(listener);
 
         cache.get(1);
-        cache = null;
     }
 }
--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -3,58 +3,59 @@ package ru.otus.cachehw;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.ref.ReferenceQueue;
-import java.util.WeakHashMap;
+import java.util.*;
 
 public class MyCache<K, V> implements HwCache<K, V> {
     public static final Logger logger = LoggerFactory.getLogger(MyCache.class);
 
     private final WeakHashMap<K, V> cache = new WeakHashMap<>();
-    private final ListenersRefSet<K, V> listeners = new ListenersRefSet<>();
-    private final ReferenceQueue<HwListener<K, V>> listenersRefQueue = new ReferenceQueue<>();
+    private final Set<HwListener<K, V>> listeners = new HashSet<>();
 
     public MyCache() {
         logger.info("constructor called");
+    }
 
-        var cleaner = new Thread(new ListenerCleaner<>(listeners, listenersRefQueue), "ListenerCleaner");
-        cleaner.start();
-
-        var cleanupMgr = new CleanupManager<>(this, cleaner);
-        new Thread(cleanupMgr, "CleanupManager").start();
+    private void notifyListeners(K key, V value, String action) {
+        logger.debug("notify listeners for key={}, value={}, action={}", key, value, action);
+        listeners.forEach((listener) -> {
+            try {
+                listener.notify(key, value, action);
+            } catch (Exception e) {
+                logger.error("exception occurred during notification", e);
+            }
+        });
     }
 
     @Override
     public void put(K key, V value) {
         logger.debug("putting key={}, value={} to the cache", key, value);
-        listeners.notify(key, value, "PUT");
+        notifyListeners(key, value, "PUT");
         cache.put(key, value);
     }
 
     @Override
     public void remove(K key) {
         logger.debug("removing key={}", key);
-        listeners.notify(key, null, "REMOVE");
+        notifyListeners(key, null, "REMOVE");
         cache.remove(key);
     }
 
     @Override
     public V get(K key) {
         logger.debug("getting key={}", key);
-        listeners.notify(key, null, "GET");
+        notifyListeners(key, null, "GET");
         return cache.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
         logger.info("adding listener={}", listener);
-        listeners.add(listener, listenersRefQueue);
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
         logger.info("removing listener={}", listener);
-        var old_size = listeners.size();
         listeners.remove(listener);
-        logger.info("size before={}, after={}", old_size, listeners.size());
     }
 }
